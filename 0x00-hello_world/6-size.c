262ce81c03d638488833ac436dfdb42014d54ae6
henryor <4754@holbertonschool.com>
2022-02-10 23:27:45 -0500

    sizes of various computes

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -7,10 +7,9 @@
 
 int main(void)
 {
-puts("\"Size of a char: 1 byte(s)
-Size of an int: 4 byte(s)
-Size of a long int: 4 byte(s)
-Size of a long long int: 8 byte(s)
-Size of a float: 4 byte(s)");
+printf("Size of a char:%d byte(s)\n", sizeof(char));
+printf("Size of an int:%d 4 byte(s)\n", sizeof(int));
+printf("size of a long int:%d 4 byte(s)\n", sizeof(long));
+puts("Size of a long long int:%d 8 byte(s)\n", sizeof(double));
 return (0);
 }
