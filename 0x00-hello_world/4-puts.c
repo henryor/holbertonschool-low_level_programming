442cb0f81c937e01a3b02876f0826452393ed967
henryor <4754@holbertonschool.com>
2022-02-10 22:58:00 -0500

    programming is like building

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  *main-description
+ *Return:0
  *Write a C program that prints exactly
  *"Programming is like building a multilingual puzzle,
  *followed by a new line.
@@ -9,5 +10,5 @@
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
-return(0);
+return (0);
 }
