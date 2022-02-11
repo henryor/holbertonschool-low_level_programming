d092c62ed10d4059f2347ec6215d3440ca03449a
henryor <4754@holbertonschool.com>
2022-02-10 23:16:28 -0500

    sizes of various computes

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *main-description
+ *Return:0
+ *Write a C program that prints the sizes of various types on the computer
+*/
+
+int main(void)
+{
+puts("\"Size of a char: 1 byte(s)
+Size of an int: 4 byte(s)
+Size of a long int: 4 byte(s)
+Size of a long long int: 8 byte(s)
+Size of a float: 4 byte(s)");
+return (0);
+}
