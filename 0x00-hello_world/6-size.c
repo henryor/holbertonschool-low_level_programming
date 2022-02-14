e60b4071f0058e8304528acc317e9afa78878b70
henryor <4754@holbertonschool.com>
2022-02-14 08:27:52 -0500

    6.Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,19 +1,20 @@
 #include<stdio.h>
-/*
- *return 0
+/**
  *main - print pc sizes
+ *Return: Always 0
  */
 int main(void)
 {
 int intType;
+long int b;
+long long int c;
 float floatType;
-double doubleType;
 char charType;
 
-/* sizeof evaluates the size of a variable*/
-printf("Size of int: %zu bytes\n", sizeof(intType));
-printf("Size of float: %zu bytes\n", sizeof(floatType));
-printf("Size of double: %zu bytes\n", sizeof(doubleType));
-printf("Size of char: %zu byte\n", sizeof(charType));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(charType));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(intType));
+printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(floatType));
 return (0);
 }
