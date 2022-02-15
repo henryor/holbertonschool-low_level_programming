fe59a6d9d0670c68b902e176676d3e1978d6184f
henryor <4754@holbertonschool.com>
2022-02-15 14:30:46 -0500

    print _putchar

---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - print _putchar
+ * use putchar
+ * Return: (0)
+ */
+int main(void)
+{
+char s = '_';
+char p = 'p';
+char u = 'u';
+char t = 't';
+char c = 'c';
+char h = 'h';
+char a = 'a';
+char r = 'r';
+putchar(s);
+putchar(p);
+putchar(u);
+putchar(t);
+putchar(c);
+putchar(h);
+putchar(a);
+putchar(r);
+putchar('\n');
+return (0);
+}
