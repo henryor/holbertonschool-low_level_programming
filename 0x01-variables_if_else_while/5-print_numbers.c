3a47944d14179d9e15e03c8845572925eb4a4871
henryor <4754@holbertonschool.com>
2022-02-14 23:07:19 -0500

    digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - print number of base 10, start from 0
+ * Return: (0)
+ */
+int main(void)
+{
+int ch;
+for (ch = 0; ch < 10; ch++)
+printf("\n");
+printf("%d", ch);
+return (0);
+}
