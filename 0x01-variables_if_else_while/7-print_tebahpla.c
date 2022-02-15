88c50485e49bd08f8ed59397cd24ce874532da43
henryor <4754@holbertonschool.com>
2022-02-14 23:42:42 -0500

    alphabet in reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print alphabet in reverse
+ * use putchar
+ * Return: (0)
+ */
+int main(void)
+{
+char ch;
+for (ch = 'z'; ch >= 'a'; ch--)
+putchar(ch);
+putchar('\n');
+return (0);
+}
