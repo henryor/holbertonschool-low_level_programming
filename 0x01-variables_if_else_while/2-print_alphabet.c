fd67054b909d30983164c5be739db3c1534923f4
henryor <4754@holbertonschool.com>
2022-02-14 21:57:12 -0500

    sometimes suffer insomnia

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - Prints the alphabet in lower case
+ *only use putchar
+ * Return: (0)
+ */
+int main(void)
+{
+char ch;
+
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+putchar(ch);
+
+return (0);
+}
