5b1911fdb97dd42bff1503b6ec60c148d6553a00
henryor <4754@holbertonschool.com>
2022-02-14 23:15:16 -0500

    digit numbers of base 10 putchar

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - print number of base 10, start from 0
+ * use putchar
+ * Return: (0)
+ */
+int main(void)
+{
+int ch;
+for (ch = 0; ch < 10; ch++)
+putchar(ch);
+putchar('\n');
+return (0);
+}
