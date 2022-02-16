729413bf2e24fd0fe682d667f768fb4d33523bbe
henryor <4754@holbertonschool.com>
2022-02-15 23:52:54 -0500

    from numer to 98

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,7 +2,7 @@
 * Auth; Henry Ortegon g
 */
 #include "main.h"
-
+#include <stdio.h>
 /**
  * print_to_98-function that prints all natural numbers from n to 98
  * user input's number prints to 98, regardless < 98 or > 98
