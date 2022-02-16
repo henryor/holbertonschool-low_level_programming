2dedbd759950d552c998a3696582f52dae884e76
henryor <4754@holbertonschool.com>
2022-02-15 22:29:32 -0500

    2. alphabetx10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,24 @@
+/*
+* Auth; Henry Ortegon g
+*/
+
+#include "main.h"
+/**
+ * print_alphabet_x10-function that prints 10 times the alphabet
+ *
+ * Return: x10 a-z
+ */
+void print_alphabet_x10(void)
+{
+int ch, i;
+i = 0;
+
+while (i < 10)
+{
+for (ch = 'a'; ch <= 'z'; ch++)
+{
+_putchar(ch);
+}
+i++;
+_putchar('\n');
+}
