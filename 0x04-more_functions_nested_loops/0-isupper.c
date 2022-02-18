bde2e7ca420aa0d444a345f1760b3141909f580d
henryor <4754@holbertonschool.com>
2022-02-18 09:39:45 -0500

    0-isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,17 @@
+#include "main"
+
+/**
+ * _supper- check for uppercase character
+ * @c c - variable
+ * Return (0)
+ * auth: Henry Ortegon G.
+ **/
+
+int _isupper(int c)
+{
+if (c >= 65 && c <= 90)
+{
+return (1);
+}
+return (0);
+}
